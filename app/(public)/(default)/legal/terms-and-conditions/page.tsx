9c3bb8063a6d90d629cc160ea7a2a8c5b656701f
Samyabrata Maji <samyabratamaji334@gmail.com>
2025-04-21 17:04:33 +0000

    feat: updated legal documents

--- app/(public)/(default)/legal/terms-and-conditions/page.tsx
+++ app/(public)/(default)/legal/terms-and-conditions/page.tsx
@@ -252,32 +252,33 @@ export default function Page() {
                 <li>Discover</li>
                 <li>PayPal</li>
                 <li>RuPay</li>
-                <li>
-                    BHIM UPI You agree to provide current, complete, and
-                    accurate purchase and account information for all purchases
-                    made via the Services. You further agree to promptly update
-                    account and payment information, including email address,
-                    payment method, and payment card expiration date, so that we
-                    can complete your transactions and contact you as needed.
-                    Sales tax will be added to the price of purchases as deemed
-                    required by us. We may change prices at any time. All
-                    payments shall be in US dollars. You agree to pay all
-                    charges at the prices then in effect for your purchases and
-                    any applicable shipping fees, and you authorize us to charge
-                    your chosen payment provider for any such amounts upon
-                    placing your order. We reserve the right to correct any
-                    errors or mistakes in pricing, even if we have already
-                    requested or received payment. We reserve the right to
-                    refuse any order placed through the Services. We may, in our
-                    sole discretion, limit or cancel quantities purchased per
-                    person, per household, or per order. These restrictions may
-                    include orders placed by or under the same customer account,
-                    the same payment method, and/or orders that use the same
-                    billing or shipping address. We reserve the right to limit
-                    or prohibit orders that, in our sole judgment, appear to be
-                    placed by dealers, resellers, or distributors.
-                </li>
+                <li>BHIM UPI</li>
             </ul>
+            <p>
+                You agree to provide current, complete, and
+                accurate purchase and account information for all purchases
+                made via the Services. You further agree to promptly update
+                account and payment information, including email address,
+                payment method, and payment card expiration date, so that we
+                can complete your transactions and contact you as needed.
+                Sales tax will be added to the price of purchases as deemed
+                required by us. We may change prices at any time. All
+                payments shall be in Indian Rupees. You agree to pay all
+                charges at the prices then in effect for your purchases and
+                any applicable shipping fees, and you authorize us to charge
+                your chosen payment provider for any such amounts upon
+                placing your order. We reserve the right to correct any
+                errors or mistakes in pricing, even if we have already
+                requested or received payment. We reserve the right to
+                refuse any order placed through the Services. We may, in our
+                sole discretion, limit or cancel quantities purchased per
+                person, per household, or per order. These restrictions may
+                include orders placed by or under the same customer account,
+                the same payment method, and/or orders that use the same
+                billing or shipping address. We reserve the right to limit
+                or prohibit orders that, in our sole judgment, appear to be
+                placed by dealers, resellers, or distributors.
+            </p>
             <h2 id="6-subscriptions">6. SUBSCRIPTIONS</h2>
             <h3 id="billing-and-renewal">Billing and Renewal</h3>
             <p>
