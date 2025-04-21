18d8dd4c7715e785bc658eeba4e1a609599a744f
Samyabrata Maji <samyabratamaji334@gmail.com>
2025-04-21 10:39:10 +0000

    feat: refund policy

---
+++ app/(public)/(default)/legal/refund-policy/page.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+    return (
+        <main className="legal__markdown mx-auto flex flex-col gap-2 px-4 py-16 lg:w-1/2 [&>ul]:flex [&>ul]:list-disc [&>ul]:flex-col [&>ul]:gap-2">
+            <h1>Refund Policy</h1>
+            <p>Last updated: April 4, 2025</p>
+            <h2>Refunds</h2>
+            <p>All sales are final and no refunds will be issued.</p>
+            <h2>Questions</h2>
+            <p>If you have any questions concerning our refund policy, please contact us at:</p>
+            <p>Budgetbee</p>
+            <p>Ganapati Tower, Gorai Road,</p>
+            <p>Gopalpur, West Bengal,</p>
+            <p>India.</p>
+            <p>Pin code: 713304</p>
+            <p>Phone: (+91)7719154350</p>
+            <p>Email: budgetbee@gmail.com</p>
+        </main>
+    )
+}
