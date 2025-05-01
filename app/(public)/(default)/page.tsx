059fccd7eb9ba0026d92e0131708d89268b0b02d
Samyabrata Maji <samyabratamaji334@gmail.com>
2025-05-01 06:32:23 +0000

    fix: landing pages

--- app/(public)/(default)/page.tsx
+++ app/(public)/(default)/page.tsx
@@ -6,79 +6,71 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Page() {
-	return (
-		<main>
-			<div className="flex flex-col justify-center gap-4 px-8 py-16 lg:items-center">
-				<h1 className="text-muted-foreground hidden items-center text-2xl lg:flex lg:text-5xl">
-					Seemlessly track your
-					<span className="text-slider text-white">
-						<span className="text-slider__word">finances</span>
-						<span className="text-slider__word">expense</span>
-						<span className="text-slider__word">subscriptions</span>
-					</span>
-				</h1>
-				<h1 className="text-2xl lg:hidden">
-					Seemlessly track your finances
-				</h1>
-				<p className="text-muted-foreground text-lg lg:w-1/2 lg:text-center">
-					Do you find managing your finances{" "}
-					<span className="text-primary-foreground">tedious</span> and
-					<span className="text-primary-foreground"> difficult</span>?
-					With budgetbee you can track your spendings and gain
-					valuable <span className="text-white">insights</span>.
-				</p>
-				<div className="mt-4 lg:flex">
-					<SignedIn>
-						<Button
-							className="group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-							asChild>
-							<Link href="/dashboard">
-								Open dashboard
-								<span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
-									🡒
-								</span>
-							</Link>
-						</Button>
-					</SignedIn>
-					<SignedOut>
-						<Button
-							className="group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-							asChild>
-							<Link href="/join">
-								Get started
-								<span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
-									🡒
-								</span>
-							</Link>
-						</Button>
-						<Button
-							className="relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[mask-composite:exclude_!important] before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
-							asChild>
-							<Link href="/blogs/getting-started">
-								Learn more
-							</Link>
-						</Button>
-					</SignedOut>
-				</div>
+    return (
+        <main>
+            <div className="flex flex-col justify-center gap-4 px-8 py-16 lg:items-center">
+                <h1 className="text-muted-foreground hidden items-center text-2xl lg:flex lg:text-5xl">
+                    Seemlessly track your
+                    <span className="text-slider text-white">
+                        <span className="text-slider__word">finances</span>
+                        <span className="text-slider__word">expense</span>
+                        <span className="text-slider__word">subscriptions</span>
+                    </span>
+                </h1>
+                <h1 className="text-2xl lg:hidden">
+                    Seemlessly track your finances
+                </h1>
+                <p className="text-muted-foreground text-lg lg:w-1/2 lg:text-center">
+                    Do you find managing your finances{" "}
+                    <span className="text-white">tedious</span> and
+                    <span className="text-white"> difficult</span>?
+                    With budgetbee you can track your spendings and gain
+                    valuable <span className="text-white">insights</span>.
+                </p>
+                <div className="mt-4 lg:flex">
+                    <SignedIn>
+                        <Button asChild>
+                            <Link href="/dashboard">
+                                Open dashboard
+                                <span className="">
+                                    🡒
+                                </span>
+                            </Link>
+                        </Button>
+                    </SignedIn>
+                    <SignedOut>
+                        <Button asChild>
+                            <Link href="/join">
+                                Get started
+                                <span>🡒</span>
+                            </Link>
+                        </Button>
+                        <Button variant="secondary" className="ml-2" asChild>
+                            <Link href="/blogs/getting-started">
+                                Learn more
+                            </Link>
+                        </Button>
+                    </SignedOut>
+                </div>
 
-				<div className="mt-4 w-full rounded-md bg-black/20 p-4 lg:w-2/3">
-					<img
-						src={BudgetbeeDashboardImg.src}
-						className="h-auto w-full rounded-md"
-					/>
-				</div>
-			</div>
+                <div className="mt-4 w-full rounded-md bg-black/20 p-4 lg:w-2/3">
+                    <img
+                        src={BudgetbeeDashboardImg.src}
+                        className="h-auto w-full rounded-md"
+                    />
+                </div>
+            </div>
 
-			{/** BENTO GRID SECTION **/}
-			<BentoGrid />
+            {/** BENTO GRID SECTION **/}
+            <BentoGrid />
 
-			<div className="flex flex-col items-center justify-center space-y-4 px-4 py-8 lg:space-y-8 lg:px-8 lg:py-24">
-				<h1 className="text-muted-foreground text-2xl lg:text-5xl">
-					One <span className="text-white">flexible pricing</span> for
-					all.
-				</h1>
-				<PricingCardGroup />
-			</div>
-		</main>
-	);
+            <div className="flex flex-col items-center justify-center space-y-4 px-4 py-8 lg:space-y-8 lg:px-8 lg:py-24">
+                <h1 className="text-muted-foreground text-2xl lg:text-5xl">
+                    One <span className="text-white">flexible pricing</span> for
+                    all.
+                </h1>
+                <PricingCardGroup />
+            </div>
+        </main>
+    );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,84 +7,84 @@ import { Menu } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuPortal,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 
 const links: { name: string; href: string | URL }[] = [
-	{ name: "Overview", href: "/overview" },
-	{ name: "Pricing", href: "/pricing" },
-	{ name: "About", href: "/about" },
-	{ name: "Support", href: "/support" },
+    { name: "Overview", href: "/overview" },
+    { name: "Pricing", href: "/pricing" },
+    { name: "About", href: "/about" },
+    { name: "Support", href: "/contact" },
 ];
 
 export function Navbar() {
-	const isDesktop = useMediaQuery("(min-width: 768px)");
-	return (
-		<div className="flex h-[56px] items-center justify-between border-b px-2 md:px-8">
-			<Link href="/" className="flex items-center gap-2">
-				<img
-					src="/images/budgetbee.svg"
-					className="flex aspect-square size-8 items-center justify-center rounded-lg"
-				/>
-				<div className="grid flex-1 text-left text-sm leading-tight">
-					<p className="truncate text-2xl">Budgetbee</p>
-				</div>
-			</Link>
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+    return (
+        <div className="flex h-[56px] items-center justify-between border-b px-2 md:px-8">
+            <Link href="/" className="flex items-center gap-2">
+                <img
+                    src="/images/budgetbee.svg"
+                    className="flex aspect-square size-8 items-center justify-center rounded-lg"
+                />
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                    <p className="truncate text-2xl">Budgetbee</p>
+                </div>
+            </Link>
 
-			{isDesktop && (
-				<React.Fragment>
-					<div className="hidden gap-4 text-sm text-[#A3A3A3] lg:flex [&>a:hover]:text-white">
-						{links.map((link, index) => (
-							<React.Fragment key={index}>
-								<Link href={link.href}>{link.name}</Link>
-							</React.Fragment>
-						))}
-					</div>
-					<div className="hidden space-x-2 lg:block">
-						<SignedOut>
-							<Button asChild size="sm" variant="secondary">
-								<Link href="/login">Login</Link>
-							</Button>
-							<Button size="sm" asChild>
-								<Link href="/join">Signup</Link>
-							</Button>
-						</SignedOut>
-						<SignedIn>
-							<Button asChild size="sm" variant="secondary">
-								<Link href="/dashboard">Dashboard</Link>
-							</Button>
-						</SignedIn>
-					</div>
-				</React.Fragment>
-			)}
+            {isDesktop && (
+                <React.Fragment>
+                    <div className="hidden gap-4 text-sm text-[#A3A3A3] lg:flex [&>a:hover]:text-white">
+                        {links.map((link, index) => (
+                            <React.Fragment key={index}>
+                                <Link href={link.href}>{link.name}</Link>
+                            </React.Fragment>
+                        ))}
+                    </div>
+                    <div className="hidden space-x-2 lg:block">
+                        <SignedOut>
+                            <Button asChild size="sm" variant="secondary">
+                                <Link href="/login">Login</Link>
+                            </Button>
+                            <Button size="sm" asChild>
+                                <Link href="/join">Signup</Link>
+                            </Button>
+                        </SignedOut>
+                        <SignedIn>
+                            <Button asChild size="sm" variant="secondary">
+                                <Link href="/dashboard">Dashboard</Link>
+                            </Button>
+                        </SignedIn>
+                    </div>
+                </React.Fragment>
+            )}
 
-			{!isDesktop && (
-				<DropdownMenu modal>
-					<DropdownMenuTrigger asChild>
-						<Button size="icon" variant="outline">
-							<Menu className="size-4" />
-						</Button>
-					</DropdownMenuTrigger>
+            {!isDesktop && (
+                <DropdownMenu modal>
+                    <DropdownMenuTrigger asChild>
+                        <Button size="icon" variant="outline">
+                            <Menu className="size-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
 
-					<DropdownMenuPortal>
-						<DropdownMenuContent>
-							{links.map((link, index) => (
-								<DropdownMenuItem key={index}>
-									<Link href={link.href}>{link.name}</Link>
-								</DropdownMenuItem>
-							))}
-							<DropdownMenuSeparator />
-							<DropdownMenuItem>Login</DropdownMenuItem>
-							<DropdownMenuItem>Signin</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenuPortal>
-				</DropdownMenu>
-			)}
-		</div>
-	);
+                    <DropdownMenuPortal>
+                        <DropdownMenuContent>
+                            {links.map((link, index) => (
+                                <DropdownMenuItem key={index}>
+                                    <Link href={link.href}>{link.name}</Link>
+                                </DropdownMenuItem>
+                            ))}
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Login</DropdownMenuItem>
+                            <DropdownMenuItem>Signin</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenuPortal>
+                </DropdownMenu>
+            )}
+        </div>
+    );
 }
