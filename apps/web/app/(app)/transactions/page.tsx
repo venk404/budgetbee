3bd8e2931aa0bb86f702347a9add132a82900270
Samya Brata Maji <samyabratamaji334@gmail.com>
2025-10-07 10:59:19 +0530

    feat: add persist() for filter-store

--- apps/web/app/(app)/transactions/page.tsx
+++ apps/web/app/(app)/transactions/page.tsx
@@ -2,17 +2,17 @@
 
 import { DeleteButton } from "@/components/delete-button";
 import {
-	FilterClear,
-	FilterDialog,
-	FilterPills,
+    FilterClear,
+    FilterDialog,
+    FilterPills,
 } from "@/components/filter-dropdown";
 import { DisplayDropdown } from "@/components/transaction-display-dropdown";
 import { TransactionsTable } from "@/components/transactions-table";
 import { Button } from "@/components/ui/button";
 import {
-	Tooltip,
-	TooltipContent,
-	TooltipTrigger,
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { useEditorStore } from "@/lib/store/editor-store";
 import { BadgeInfo, Check, SquarePen, X } from "lucide-react";
@@ -20,75 +20,79 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function Page() {
-	const isEditing = useEditorStore(s => s.is_editing);
-	const formStates = useForm({});
+    const isEditing = useEditorStore(s => s.is_editing);
+    const formStates = useForm();
 
-	return (
-		<div>
-			<div>
-				<div className="flex items-start justify-between border-b p-2">
-					<div className="flex flex-wrap gap-2">
-						<FilterDialog />
-						<FilterClear />
-						<FilterPills />
-					</div>
+    return (
+        <div>
+            <div>
+                <div className="flex items-start justify-between border-b p-2">
+                    <div className="flex flex-wrap gap-2">
+                        <FilterDialog />
+                        <FilterClear />
+                        <FilterPills />
+                    </div>
 
-					<div className="flex gap-2">
-						<DeleteButton />
+                    <div className="flex gap-2">
+                        <DeleteButton />
 
-						{isEditing ?
-							<React.Fragment>
-								<Button
-									size="sm"
-									className="border"
-									variant="secondary">
-									<Check /> Save
-								</Button>
+                        {isEditing ?
+                            <React.Fragment>
+                                <Button
+                                    size="sm"
+                                    className="border"
+                                    variant="secondary"
+                                    onClick={() => {
+                                        formStates.handleSubmit(formStates.getValues())();
+                                    }}
+                                >
+                                    <Check /> Save
+                                </Button>
 
-								<Button
-									size="sm"
-									className="border"
-									variant="secondary"
-									onClick={() => {
-										useEditorStore.setState({
-											is_editing: false,
-										});
-										formStates.reset();
-									}}>
-									<X /> Cancel
-								</Button>
-							</React.Fragment>
-						:	<Tooltip delayDuration={750}>
-								<TooltipTrigger asChild>
-									<Button
-										size="sm"
-										className="border"
-										variant="secondary"
-										onClick={() =>
-											useEditorStore.setState({
-												is_editing: true,
-											})
-										}>
-										<SquarePen /> Edit
-									</Button>
-								</TooltipTrigger>
-								<TooltipContent className="bg-accent border p-2 shadow-xl">
-									<div className="flex items-center justify-center gap-2">
-										<BadgeInfo className="text-muted-foreground size-4" />
-										<p>Edit transactions.</p>
-									</div>
-								</TooltipContent>
-							</Tooltip>
-						}
+                                <Button
+                                    size="sm"
+                                    className="border"
+                                    variant="secondary"
+                                    onClick={() => {
+                                        useEditorStore.setState({
+                                            is_editing: false,
+                                        });
+                                        formStates.reset();
+                                    }}>
+                                    <X /> Cancel
+                                </Button>
+                            </React.Fragment>
+                            : <Tooltip delayDuration={750}>
+                                <TooltipTrigger asChild>
+                                    <Button
+                                        size="sm"
+                                        className="border"
+                                        variant="secondary"
+                                        onClick={() =>
+                                            useEditorStore.setState({
+                                                is_editing: true,
+                                            })
+                                        }>
+                                        <SquarePen /> Edit
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent className="bg-accent border p-2 shadow-xl">
+                                    <div className="flex items-center justify-center gap-2">
+                                        <BadgeInfo className="text-muted-foreground size-4" />
+                                        <p>Edit transactions.</p>
+                                    </div>
+                                </TooltipContent>
+                            </Tooltip>
+                        }
 
-						<DisplayDropdown />
-					</div>
-				</div>
-			</div>
+                        <DisplayDropdown />
+                    </div>
+                </div>
+            </div>
 
-			<div className="p-4">
-				<TransactionsTable formStates={formStates} />
-			</div>
-		</div>
-	);
+            <div className="p-4">
+                <TransactionsTable formStates={formStates} />
+            </div>
+        </div>
+    );
 }
--- apps/web/lib/store/filter-store.ts
+++ apps/web/lib/store/filter-store.ts
@@ -1,5 +1,6 @@
 import { PostgrestFilterBuilder } from "@supabase/postgrest-js";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export type FilterState = Record<FilterFields, string[]>;
 export type FilterFields =
@@ -86,129 +87,148 @@ export type FilterStore = {
 	) => PostgrestFilterBuilder<{ PostgrestVersion: "12" }, any, any, any>;
 };
 
-export const useFilterStore = create<FilterStore>()((set, get) => ({
-	filter_stack: [],
+export const useFilterStore = create<FilterStore>()(
+	persist(
+		(set, get) => ({
+			filter_stack: [],
 
-	filter_add: (field, operation, values, id) => {
-		const stack = get().filter_stack;
+			filter_add: (field, operation, values, id) => {
+				const stack = get().filter_stack;
 
-		console.log(stack);
-
-		let idx = stack.findIndex(x => x.id === id);
-		if (stack.length >= MAX_FILTER_STACK_SIZE) return;
-		if (idx === -1) stack.push({ field, operation, values, id });
-		else {
-			const item = stack[idx];
-			item.operation = operation;
-			item.values = values;
-			item.id = id;
-			stack[idx] = item;
-		}
-		set({
-			filter_stack: [...stack.filter(x => x.values.length > 0)],
-		});
-	},
-
-	filter_remove: idx => {
-		const stack = get().filter_stack;
-		if (idx < 0 || idx >= stack.length) return;
-		stack.splice(idx, 1);
-		set({ filter_stack: [...stack] });
-	},
-
-	filter_toggle: (field, operation, value, id) => {
-		const idx = get().filter_stack.findIndex(x => x.id === id);
-		if (idx < 0) return get().filter_add(field, operation, [value], id);
-		let original_operation = get().filter_stack[idx].operation;
-		let values = get().filter_stack[idx].values;
-		const idx2 = values.findIndex(x => x.id === value.id);
-		if (idx2 < 0) values = [...values, value];
-		else values = [...values.slice(0, idx2), ...values.slice(idx2 + 1)];
-		return get().filter_add(field, original_operation, [...values], id);
-	},
-
-	filter_clear: () => set({ filter_stack: [] }),
-
-	filter_apply: q => {
-		const properties_map: Record<FilterFields, string> = {
-			amount: "amount",
-			category: "category_id",
-			status: "status",
-			created_at: "created_at",
-			updated_at: "updated_at",
-			transaction_date: "transaction_date",
-		};
-
-		for (const { field, operation, values } of get().filter_stack) {
-			const mapped_field = properties_map[field];
-
-			// check if operation is allowed for this field
-			if (!allowed_operations_map[field].includes(operation)) continue;
-
-			if (field === "amount") {
-				if (values.length < 1) continue;
-				const value = Number(values[0].value);
-				if (Number.isNaN(value)) continue;
-				// @ts-ignore
-				q[operation](mapped_field, value);
-				continue;
-			}
-
-			if (field === "category" || field === "status") {
-				if (operation === "is empty") {
-					q.is(mapped_field, null);
-					continue;
+				let idx = stack.findIndex(x => x.id === id);
+				if (stack.length >= MAX_FILTER_STACK_SIZE) return;
+				if (idx === -1) stack.push({ field, operation, values, id });
+				else {
+					const item = stack[idx];
+					item.operation = operation;
+					item.values = values;
+					item.id = id;
+					stack[idx] = item;
 				}
+				set({
+					filter_stack: [...stack.filter(x => x.values.length > 0)],
+				});
+			},
 
-				if (values.length < 1) continue;
-				const x = values.map(x => x.value.toString());
+			filter_remove: idx => {
+				const stack = get().filter_stack;
+				if (idx < 0 || idx >= stack.length) return;
+				stack.splice(idx, 1);
+				set({ filter_stack: [...stack] });
+			},
 
-				if (operation === "is") {
-					q.in(mapped_field, x);
-					continue;
+			filter_toggle: (field, operation, value, id) => {
+				const idx = get().filter_stack.findIndex(x => x.id === id);
+				if (idx < 0)
+					return get().filter_add(field, operation, [value], id);
+				let original_operation = get().filter_stack[idx].operation;
+				let values = get().filter_stack[idx].values;
+				const idx2 = values.findIndex(x => x.id === value.id);
+				if (idx2 < 0) values = [...values, value];
+				else
+					values = [
+						...values.slice(0, idx2),
+						...values.slice(idx2 + 1),
+					];
+				return get().filter_add(
+					field,
+					original_operation,
+					[...values],
+					id,
+				);
+			},
+
+			filter_clear: () => set({ filter_stack: [] }),
+
+			filter_apply: q => {
+				const properties_map: Record<FilterFields, string> = {
+					amount: "amount",
+					category: "category_id",
+					status: "status",
+					created_at: "created_at",
+					updated_at: "updated_at",
+					transaction_date: "transaction_date",
+				};
+
+				for (const { field, operation, values } of get().filter_stack) {
+					const mapped_field = properties_map[field];
+
+					// check if operation is allowed for this field
+					if (!allowed_operations_map[field].includes(operation))
+						continue;
+
+					if (field === "amount") {
+						if (values.length < 1) continue;
+						const value = Number(values[0].value);
+						if (Number.isNaN(value)) continue;
+						// @ts-ignore
+						q[operation](mapped_field, value);
+						continue;
+					}
+
+					if (field === "category" || field === "status") {
+						if (operation === "is empty") {
+							q.is(mapped_field, null);
+							continue;
+						}
+
+						if (values.length < 1) continue;
+						const x = values.map(x => x.value.toString());
+
+						if (operation === "is") {
+							q.in(mapped_field, x);
+							continue;
+						}
+
+						if (operation === "is not") {
+							q.not(mapped_field, "in", "(" + x.join(",") + ")");
+							continue;
+						}
+
+						continue;
+					}
+
+					if (
+						field === "created_at" ||
+						field === "updated_at" ||
+						field === "transaction_date"
+					) {
+						if (values.length < 1) continue;
+						const value =
+							values[0].value instanceof Date ?
+								values[0].value
+							:	null;
+
+						if (operation === "from") {
+							q.gte(mapped_field, value);
+							continue;
+						}
+
+						if (operation === "to") {
+							q.lte(mapped_field, value);
+							continue;
+						}
+
+						if (operation === "between") {
+							const d1 = value;
+							const d2 =
+								(
+									values.length >= 2 &&
+									values[1].value instanceof Date
+								) ?
+									values[1].value
+								:	null;
+							q.gt(mapped_field, d1);
+							q.lt(mapped_field, d2);
+						}
+					}
 				}
-
-				if (operation === "is not") {
-					q.not(mapped_field, "in", "(" + x.join(",") + ")");
-					continue;
-				}
-
-				continue;
-			}
-
-			if (
-				field === "created_at" ||
-				field === "updated_at" ||
-				field === "transaction_date"
-			) {
-				if (values.length < 1) continue;
-				const value =
-					values[0].value instanceof Date ? values[0].value : null;
-
-				if (operation === "from") {
-					q.gte(mapped_field, value);
-					continue;
-				}
-
-				if (operation === "to") {
-					q.lte(mapped_field, value);
-					continue;
-				}
-
-				if (operation === "between") {
-					const d1 = value;
-					const d2 =
-						values.length >= 2 && values[1].value instanceof Date ?
-							values[1].value
-						:	null;
-					q.gt(mapped_field, d1);
-					q.lt(mapped_field, d2);
-				}
-			}
-		}
-		return q;
-	},
-}));
+				return q;
+			},
+		}),
+		{ name: "tr_filter" },
+	),
+);
 
 export const serializeFilterStack = (stack: FilterStackItem[]) => {
 	return stack.map(stackItem => {
